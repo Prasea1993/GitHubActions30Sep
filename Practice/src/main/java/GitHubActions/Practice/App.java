2a0ba58aab4d5c5e17377da6a70707b1882f5d65
Prashanth Samudrala <77669873+psamudrala-nd-wahbe@users.noreply.github.com>
2022-09-30 17:06:49 +0530

    First commit

---
+++ Practice/src/main/java/GitHubActions/Practice/App.java
@@ -0,0 +1,13 @@
+package GitHubActions.Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldaas!" );
+    }
+}
---
+++ Practice/src/test/java/GitHubActions/Practice/AppTest.java
@@ -0,0 +1,16 @@
+package GitHubActions.Practice;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple app.
+ */
+public class AppTest
+{
+    
+    @Test
+    public void testApp()
+    {
+        System.out.println("AppTest class");
+    }
+}
