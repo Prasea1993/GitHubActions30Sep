0ed88e97b1208f72f2b29900920378283cad7244
prashanths <prashanths@backbase.com>
2023-05-12 22:39:02 +0530

    This is my first commit in Maysprint_Prashanth branch

--- Practice/src/main/java/GitHubActions/Practice/App.java
+++ Practice/src/main/java/GitHubActions/Practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldaas!" );
+        System.out.println( "Hello Worldaas! which is in MaySprint branch" );
     }
 }
